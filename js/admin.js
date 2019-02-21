60c9fb310cb8017eac7971d7924a66b263c1fcc5
SupperPigger <supperpigger@SupperPiggerdeMacBook-Pro.local>
2019-02-22 00:20:32 +0800

    添加主体框架&一些假数据

--- js/admin.js
+++ js/admin.js
@@ -1,11 +1,21 @@
-window.onload = function () {
-    console.log('onload')
+window.onload = function(){
+    // console.log(sqrt)console.log('onload')
     var pageHeight = $(document).height();
-    $('.hotel-login-box').css('height', pageHeight + 'px');
+    $('.add-hotel-box').css('height', pageHeight + 'px');
+    var xx = getUrlParam('userName');
+    console.log(xx)
+     if (xx == 'admin') {
+
+    } else {
+         window.location.href='index.html';
+    }
 }
 
-var userName = ''
-var userPassword = ''
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r != null) return unescape(r[2]); return null; //返回参数值
+}
 
 let drf = [
     {
@@ -128,7 +138,7 @@ let ztf = [
     },
     {
         Title:"ztf",
-        Message:'总统房', 
+        Message:'总统房',
         Prise:800,
         V_prise:750,
         Img:'',
@@ -165,11 +175,10 @@ for (let i = 0; i < allHotel.length; i++) {
     <p class="hotel-prise" style="letter-spacing: 28px">价格</p> <span class="hotel-prises">￥${allHotel[i].Prise}</span>
     <p class="hotel-prise-vip">vip-价格</p> <span class="hotel-prises-vip">￥${allHotel[i].V_prise}</span>
     <div class="hotel-right" data-id="${allHotel[i].Num}">
-        预约
+        删除
     </div>
 </div>`
 }
-//循环动态输出房间
 for (let i = 0; i < drf.length; i++) {
     onlyMsg = onlyMsg + `<div class="hotel-text">
     <div class="hotel-img">
@@ -179,7 +188,7 @@ for (let i = 0; i < drf.length; i++) {
     <p class="hotel-prise" style="letter-spacing: 28px">价格</p> <span class="hotel-prises">￥${drf[i].Prise}</span>
     <p class="hotel-prise-vip">vip-价格</p> <span class="hotel-prises-vip">￥${drf[i].V_prise}</span>
     <div class="hotel-right" data-id="${drf[i].Num}">
-        预约
+        删除
     </div>
 </div>`
 }
@@ -192,7 +201,7 @@ for (let i = 0; i < srf.length; i++) {
     <p class="hotel-prise" style="letter-spacing: 28px">价格</p> <span class="hotel-prises">￥${srf[i].Prise}</span>
     <p class="hotel-prise-vip">vip-价格</p> <span class="hotel-prises-vip">￥${srf[i].V_prise}</span>
     <div class="hotel-right" data-id="${srf[i].Num}">
-        预约
+        删除
     </div>
 </div>`
 }
@@ -205,7 +214,7 @@ for (let i = 0; i < ztf.length; i++) {
     <p class="hotel-prise" style="letter-spacing: 28px">价格</p> <span class="hotel-prises">￥${ztf[i].Prise}</span>
     <p class="hotel-prise-vip">vip-价格</p> <span class="hotel-prises-vip">￥${ztf[i].V_prise}</span>
     <div class="hotel-right" data-id="${ztf[i].Num}">
-        预约
+        删除
     </div>
 </div>`
 }
@@ -213,47 +222,6 @@ $('.hotel-all').html(allMsg)
 $('.hotel-once').html(onlyMsg)
 $('.hotel-double').html(doubleMsg)
 $('.hotel-vip').html(VMsg)
-console.log(allMsg)
-
-
-$('.login').click(function (e) {
-    e.preventDefault();
-    $('.hotel-login').show();
-    $('.hotel-register').hide();
-    $('.hotel-login-box').show();
-});
-$('.register').click(function (e) {
-    e.preventDefault();
-    $('.hotel-login').hide();
-    $('.hotel-register').show();
-    $('.hotel-login-box').show();
-});
-
-$('.hotel-login-box .box-close').click(function (e) {
-    e.preventDefault();
-    $('.hotel-login-box').hide();
-});
-
-$('.hotel-header-reserve').click(function (e) {
-    e.preventDefault();
-    $('.hotel-header-reserve').css('color', '#CCCCCC');
-    $('.hotel-header-index').css('color', 'black');
-    $('.hotel-index-msg').hide();
-    $('.hotel-reserve-msg').show();
-});
-
-$('.hotel-header-index').click(function (e) {
-    e.preventDefault();
-    $('.hotel-header-reserve').css('color', 'black');
-    $('.hotel-header-index').css('color', '#CCCCCC');
-    $('.hotel-index-msg').show();
-    $('.hotel-reserve-msg').hide();
-});
-
-$('.hotel-header-name').click(function (e) {
-    e.preventDefault();
-    window.location.href = "indexAdmin.html?userName="+userName;
-});
 
 $(".hotel-list-all").click(function (e) {
     e.preventDefault();
@@ -283,36 +251,39 @@ $(".hotel-list-vip").click(function (e) {
     $('.hotel-all,.hotel-once,.hotel-double,.hotel-vip').hide();
     $('.hotel-vip').show();
 });
-//判断登录信息。如果用户名和密码是对的显示，否则隐藏
-$('.login-submit').click(function (e) { 
-    e.preventDefault();
-    userName = $('.login-username').val();
-    userPassword = $('.login-password').val();
-    console.log(userName, userPassword)
-    if (userName == 'admin' && userPassword == '123456') {
-        $('.hotel-user-login-yes').show();
-        $('.hotel-user-login-none').hide();
-        $('.hotel-login-box').hide();
-        $('.user-name').html(userName);
-        $('.user-mail').html('123456789@qq.com');
-        $('.user-phone').html('13333333333');
-        $('.hotel-user-login-yes').show();
 
-    } else if(userName == 'npm' && userPassword == '123456'){
-        $('.hotel-user-login-none').hide();
-        $('.hotel-login-box').hide();
-        $('.user-name').html(userName);
-        $('.user-mail').html('123456789@qq.com');
-        $('.user-phone').html('13333333333');
-        $('.hotel-user-login-yes').hide();
-	    $('.hotel-user-login-yes1').show();
-    }else {
-        alert('用户名&密码错误，请重试');
-    }
+$('.add-hotel').click(function (e) { 
+    e.preventDefault();
+    $('.add-hotel-box').show();
 });
-//点击弹出房间号
+$('.add-box .close').click(function (e) { 
+    e.preventDefault();
+    $('.add-hotel-box').hide();
+});
+
 $('.hotel-right').click(function (e) { 
     e.preventDefault();
     var num = $(this).attr('data-id');
-    alert(num+'号房间预订成功')
+    alert('已删除'+num+'号房间');
+    $(this).parent().hide();
+});
+//判断状态值，如果状态为0赋值是单人间
+$('.add-hotel-right').click(function (e) { 
+    e.preventDefault();
+    var thisStyle = $('.hotel-style').val();
+    if (thisStyle == 0) {
+        thisStyle = '单人间'
+    } else if (thisStyle == 1) {
+        thisStyle = '双人间'
+    } else if (thisStyle == 2) {
+        thisStyle = '总统房'
+    }
+    console.log(thisStyle)
+    var thisPrise = $('.hotel-prise-num').val();
+    var thisVipPrise = $('.hotel-prise-num-vip').val();
+    if (thisPrise == '' || thisVipPrise == '') {
+        alert('请填写完整');
+    } else {
+        alert('已添加新的'+thisStyle)
+    }
 });
